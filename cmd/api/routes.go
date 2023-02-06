d27a7fb6aa3d42be0226b603f9ad94303a35dd19
standvpmnt <standvpmnt@gmail.com>
2023-02-06 14:57:47 +0530

    Implement authentication with stateful token

--- cmd/api/routes.go
+++ cmd/api/routes.go
@@ -23,5 +23,7 @@ func (app *application) routes() http.Handler {
 	router.HandlerFunc(http.MethodPost, "/v1/users", app.registerUserHandler)
 	router.HandlerFunc(http.MethodPut, "/v1/users/activated", app.activateUserHandler)
 
-	return app.recoverPanic(app.rateLimit(router))
+	router.HandlerFunc(http.MethodPost, "/v1/tokens/authentication", app.createAuthenticationTokenHandler)
+
+	return app.recoverPanic(app.rateLimit(app.authenticate(router)))
 }
--- internal/data/users.go
+++ internal/data/users.go
@@ -11,6 +11,8 @@ import (
 	"greenlight.standvpmnt.github.com/internal/validator"
 )
 
+var AnonymousUser = &User{}
+
 type User struct {
 	ID        int64     `json:"id"`
 	CreatedAt time.Time `json:"created_at"`
@@ -24,6 +26,10 @@ type User struct {
 type password struct {
 	plaintext *string
 	hash      []byte
+}
+
+func (u *User) IsAnonymous() bool {
+	return u == AnonymousUser
 }
 
 func (p *password) Set(plaintextPassword string) error {
