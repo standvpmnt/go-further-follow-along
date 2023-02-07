b89615a21dbbfd1545a5d7329641796dc70e386a
standvpmnt <standvpmnt@gmail.com>
2023-02-07 09:20:11 +0530

    Add cors handling middleware for simple cors and preflight cors requests

--- cmd/api/middleware.go
+++ cmd/api/middleware.go
@@ -173,3 +173,37 @@ func (app *application) requirePermission(code string, next http.HandlerFunc) ht
 	}
 	return app.requireActivatedUser(fn)
 }
+
+func (app *application) enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Vary", "Origin")
+
+		w.Header().Add("Vary", "Access-Control-Request-Method")
+
+		origin := r.Header.Get("Origin")
+
+		if origin != "" {
+			for i := range app.config.cors.trustedOrigins {
+				if origin == app.config.cors.trustedOrigins[i] {
+					w.Header().Set("Access-Control-Allow-Origin", origin)
+
+					if r.Method == http.MethodOptions && r.Header.Get("Access-Control-Request-Method") != "" {
+						w.Header().Set("Access-Control-Allow-Methods", "OPTIONS, PUT, PATCH, DELETE")
+						w.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type")
+
+						// w.Header().Set("Access-Control-Max-Age", "60")
+
+						w.WriteHeader(http.StatusOK)
+						return
+					}
+					break
+				}
+			}
+		}
+		// w.Header().Set("Access-Control-Allow-Origin", "*")
+
+		// w.Header().Set("Access-Control-Allow-Credentials", true)
+
+		next.ServeHTTP(w, r)
+	})
+}
